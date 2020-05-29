79ea1b14306a9d6542e5e89af35369e6249d5f4c
cle5eland <clthomps@microsoft.com>
2020-05-29 09:49:25 -1000

    configure nvm

--- remote/setup.sh
+++ remote/setup.sh
@@ -4,6 +4,9 @@ cd ~
 # Install Node
 echo "installing nodejs..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 echo "Done!"
 
