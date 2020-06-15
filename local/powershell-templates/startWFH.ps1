7483d1935310e02dba8cba66bb8cdac74f6470b6
cle5eland <clthomps@microsoft.com>
2020-06-15 16:57:13 -0700

    nit

--- local/powershell-templates/startWFH.ps1
+++ local/powershell-templates/startWFH.ps1
@@ -5,5 +5,5 @@ $machineName=$args[2]
 $rg=$args[3]
 $subscription=$args[4]
 az vm start --name $machineName -g $rg --subscription $subscription
-sh ./proxy-port.sh $alias $ip
-sh ./ssh.sh $alias $ip
+./proxy-port.ps1 $alias $ip
+./ssh.ps1 $alias $ip
